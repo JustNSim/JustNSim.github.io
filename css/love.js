e00f17b015b07a4b773a4186e226bc3312c0dc20
JustNSim <1174900982@qq.com>
2022-04-29 15:48:22 +0800

    题目修改

--- css/love.js
+++ css/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Come Baby", 23, 10);
+            ctx.fillText("click the heart", 23, 10);
             ctx.restore();
         },
         clear: function() {
@@ -386,7 +386,7 @@
             var rec = s.record[k || "move"];
             var point = rec.point,
                 image = rec.image,
-                speed = rec.speed || 10,
+                speed = rec.speed/3 || 3,
                 width = rec.width,
                 height = rec.height; 
 
